e115195c93c7b68e7cdc1053858230a63825c323
Christian-Kammerath <Christian-Kammerath@gmx.de>
2024-01-28 20:58:14 +0100

    Update Ui

--- KingOfSortedFiles/obj/Debug/net8.0/KingOfSortedFiles.AssemblyInfo.cs
+++ KingOfSortedFiles/obj/Debug/net8.0/KingOfSortedFiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KingOfSortedFiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b448542df2184a53a480b794f119ff202ec19eb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9be76a482338c9864931abff9231140731ad752")]
 [assembly: System.Reflection.AssemblyProductAttribute("KingOfSortedFiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KingOfSortedFiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
