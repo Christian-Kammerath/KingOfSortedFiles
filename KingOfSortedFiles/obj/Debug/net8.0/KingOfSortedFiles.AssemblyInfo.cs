49fe0c4d22636c05e2c1e2e8dc3075ec37b76881
Christian-Kammerath <Christian-Kammerath@gmx.de>
2024-02-27 17:24:06 +0100

    Bug Fix

--- KingOfSortedFiles/obj/Debug/net8.0/KingOfSortedFiles.AssemblyInfo.cs
+++ KingOfSortedFiles/obj/Debug/net8.0/KingOfSortedFiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KingOfSortedFiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41b924a4f3bada333cf210e161601ad782022171")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df63a0a655e4cfa5f99fae220bc490ea60a89a3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("KingOfSortedFiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KingOfSortedFiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
